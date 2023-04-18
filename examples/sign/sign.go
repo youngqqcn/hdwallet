fc17a49feef6d737c0cea42d24fa4e4c0dfe1163
yqq <youngqqcn@gmail.com>
2023-04-18 20:09:18 +0800

    fix private key curve is not secp256k1

---
+++ examples/sign/sign.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/big"
+
+	// "github.com/davecgh/go-spew/spew"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/youngqqcn/hdwallet"
+)
+
+func main() {
+	mnemonic := "tag volcano eight thank tide danger coast health above argue embrace heavy"
+	wallet, err := hdwallet.NewFromMnemonic(mnemonic)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	path := hdwallet.MustParseDerivationPath("m/44'/60'/0'/0/0")
+	account, err := wallet.Derive(path, true)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	nonce := uint64(0)
+	value := big.NewInt(1000000000000000000)
+	toAddress := common.HexToAddress("0x0")
+	gasLimit := uint64(21000)
+	gasPrice := big.NewInt(21000000000)
+	var data []byte
+
+	tx := types.NewTransaction(nonce, toAddress, value, gasLimit, gasPrice, data)
+	signedTx, err := wallet.SignTx(account, tx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("txhash: %v", signedTx.Hash().String())
+
+	// spew.Dump(signedTx)
+}
---
+++ go.mod
@@ -0,0 +1,19 @@
+module github.com/youngqqcn/hdwallet
+
+go 1.20
+
+require (
+	github.com/btcsuite/btcd v0.23.4
+	github.com/btcsuite/btcd/btcutil v1.1.0
+	github.com/ethereum/go-ethereum v1.11.5
+	github.com/tyler-smith/go-bip39 v1.1.0
+)
+
+require (
+	github.com/btcsuite/btcd/btcec/v2 v2.3.2 // indirect
+	github.com/btcsuite/btcd/chaincfg/chainhash v1.0.2 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.2.0 // indirect
+	github.com/holiman/uint256 v1.2.0 // indirect
+	golang.org/x/crypto v0.8.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+)
